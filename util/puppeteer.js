6c69b417b24e76622305fda3e6c596e87312b662
Josiah360 <josiaholajide@gmail.com>
2023-02-09 12:19:01 +0100

    Insert chromium from chrome-aws-lambda

--- util/puppeteer.js
+++ util/puppeteer.js
@@ -1,9 +1,14 @@
 import puppeteer from "puppeteer-core";
-
+import Chromium from "chrome-aws-lambda";
 const htmlToPdf = async (html) => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
+  const browser = await puppeteer.launch(
+    {
+          args: Chromium.args,
+          executablePath: await Chromium.executablePath,
+          headless: Chromium.headless,
+        }
+
+  );
   const page = await browser.newPage();
   await page.setContent(html);
   const pdf = await page.pdf({ format: 'A4', printBackground: true });
