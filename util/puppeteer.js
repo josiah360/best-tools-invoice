e805fc5f053dd907486c091641a200534e1c7973
Josiah360 <josiaholajide@gmail.com>
2023-02-09 16:19:02 +0100

    Download instead of show

--- util/puppeteer.js
+++ util/puppeteer.js
@@ -1,16 +1,24 @@
 import Chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer";
 
 const htmlToPdf = async (html) => {
-  const browser = await Chromium.puppeteer.launch(
-    {
-      args: [...Chromium.args, "--hide-scrollbars", "--disable-web-security"],
-      executablePath: await Chromium.executablePath,
-      headless: Chromium.headless,
-      ignoreHTTPSErrors: true,
-      ignoreDefaultArgs: ['--disable-extensions'],
-    }
+  const production = true
+  let browser
 
-  );
+  if(production) {
+    browser = await Chromium.puppeteer.launch(
+      {
+        args: [...Chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        executablePath: await Chromium.executablePath,
+        headless: Chromium.headless,
+        ignoreHTTPSErrors: true,
+        ignoreDefaultArgs: ['--disable-extensions'],
+      }
+    );
+  } else {
+    browser = await puppeteer.launch();
+  }
+
   const page = await browser.newPage();
   await page.setContent(html);
   const pdf = await page.pdf({ format: 'A4', printBackground: true });
