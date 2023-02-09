3cd18ac4d34184b377a811598b1bf524c618f45a
Josiah360 <josiaholajide@gmail.com>
2023-02-09 11:43:46 +0100

    remove puppereer

--- util/puppeteer.js
+++ util/puppeteer.js
@@ -1,7 +1,7 @@
-import puppeteer from "puppeteer";
+import { launch } from 'chrome-aws-lambda';
 
 const htmlToPdf = async (html) => {
-  const browser = await puppeteer.launch({
+  const browser = await launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
   const page = await browser.newPage();
