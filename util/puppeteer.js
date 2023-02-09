45897b76a32d296a2f10091e91476067c67ee7e7
Josiah360 <josiaholajide@gmail.com>
2023-02-09 14:02:07 +0100

    Implement puppeteer

--- util/puppeteer.js
+++ util/puppeteer.js
@@ -1,12 +1,14 @@
-import { puppeteer } from "chrome-aws-lambda";
 import Chromium from "chrome-aws-lambda";
+
 const htmlToPdf = async (html) => {
-  const browser = await puppeteer.launch(
+  const browser = await Chromium.puppeteer.launch(
     {
-          args: Chromium.args,
-          executablePath: await Chromium.executablePath,
-          headless: Chromium.headless,
-        }
+      args: Chromium.args,
+      defaultViewport: Chromium.defaultViewport,
+      executablePath: await Chromium.executablePath,
+      headless: Chromium.headless,
+      ignoreHTTPSErrors: true,
+    }
 
   );
   const page = await browser.newPage();
